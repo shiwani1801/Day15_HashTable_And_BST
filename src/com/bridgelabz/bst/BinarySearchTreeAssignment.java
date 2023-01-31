3b06a14693fc6f6e4954c2bac4b0b79d4c4f17c8
shiwani <shiwanipaunikar1@gmail.com>
2023-01-31 11:33:08 -0800

    Added:Ability to create BST and calculate its size

--- src/com/bridgelabz/bst/BinarySearchTreeAssignment.java
+++ src/com/bridgelabz/bst/BinarySearchTreeAssignment.java
@@ -1,6 +1,10 @@
 package com.bridgelabz.bst;
+//Use Case 1 --> Ability to create a BST by adding 56 and then adding 30 & 70.
+//Use Case 2 --> Ability to create a BST, check if all are added and calculate size and height of the BST.
+import java.util.Arrays;
 
 public class BinarySearchTreeAssignment <T extends Comparable<T>>{
+
     //Nested class
     static class MyBinaryNode<T> {
 
@@ -17,6 +21,8 @@ public class BinarySearchTreeAssignment <T extends Comparable<T>>{
             this.right = null;
         }
     }
+
+    Integer[] passValue = {56,30,70,22,40,11,3,16,60,95,65,63,67};
 
     MyBinaryNode root = null;
 
@@ -62,19 +68,96 @@ public class BinarySearchTreeAssignment <T extends Comparable<T>>{
         }
 
         inorderUtil(node.left);
-        System.out.print(node.data + " ");
+        System.out.print(node.data + "   ");
         inorderUtil(node.right);
+    }
+
+    //Calculating Height of BST
+    public int calculateHeight(MyBinaryNode node)
+    {
+        if (node == null)
+        {
+            return 0;
+        }
+        else
+        {
+            //Computing depth of each subtree
+            int leftDepth = calculateHeight(node.left);
+            int rightDepth = calculateHeight(node.right);
+
+            if (leftDepth > rightDepth)
+            {
+                return (leftDepth + 1);
+            }
+            else
+            {
+                return (rightDepth + 1);
+            }
+        }
+    }
+
+    //Calculating the size of BST
+    public void calculateSize()
+    {
+        System.out.println("The size of the Binary Search Tree is : "+size(root));
+    }
+    //Compute the size recursively
+    public int size(MyBinaryNode node)
+    {
+        if (node == null)
+            return 0;
+        else
+            return (size(node.left) + 1 + size(node.right));
+    }
+
+    //Check all the elements from BST
+    public boolean ifNodeExists( MyBinaryNode node, T key)
+    {
+        if (node == null)
+            return false;
+
+        if (node.data == key)
+            return true;
+
+        // then recur on left subtree /
+        boolean res1 = ifNodeExists(node.left, key);
+
+        // node found, no need to look further
+        if(res1)
+            return true;
+
+        // node is not found in left,
+        // so recur on right subtree /
+        boolean res2 = ifNodeExists(node.right, key);
+
+        return res2;
     }
 
     public static void main(String[] args) {
         BinarySearchTreeAssignment tree = new BinarySearchTreeAssignment();
 
         //Adding values to BST
-        tree.addToBST(56);
-        tree.addToBST(30);
-        tree.addToBST(70);
+        for (int i = 0; i < tree.passValue.length; i++)
+        {
+            tree.addToBST(tree.passValue[i]);
+        }
 
         //Printing all the values of BST
         tree.traversal();
+        System.out.println();
+
+        //Calculating the size of BST
+        tree.calculateSize();
+
+        //Calculating the height of BST
+        System.out.println("The height of the BST is : "+tree.calculateHeight(tree.root));
+
+        //Checking if all values are present in BST
+        boolean[] flag = new boolean[tree.passValue.length];
+        for (int i = 0; i < tree.passValue.length; i++)
+        {
+            flag[i] = tree.ifNodeExists(tree.root,tree.passValue[i]);
+            System.out.println(tree.passValue[i]+" is present |  "+flag[i]);
+        }
     }
 }
